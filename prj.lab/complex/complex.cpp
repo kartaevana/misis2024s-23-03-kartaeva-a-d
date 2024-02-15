20c0e2f08de8216b7d147e608f14fd9c5182028b
Anna <144015476+kartaevana@users.noreply.github.com>
2024-02-15 23:54:36 +0300

    Update complex.cpp

--- prj.lab/complex/complex.cpp
+++ prj.lab/complex/complex.cpp
@@ -1,5 +1,9 @@
-#include <complex/complex.hpp>
+#include "complex.hpp"
 
+Complex Complex::operator-() const noexcept { return Complex(-re, -im); }
+
+bool Complex::operator==(const Complex& rhs) const noexcept { return true; }
+bool Complex::operator!=(const Complex& rhs) const noexcept { return true; }
 
 Complex::Complex(const double real)
     : Complex(real, 0.0)
@@ -11,35 +15,38 @@ Complex::Complex(const double real, const double imaginary)
 {
 }
 
-Complex& Complex::operator+= (const Complex& rhs)
+Complex& Complex::operator+=(const Complex& rhs)
 {
     re += rhs.re;
     im += rhs.im;
     return *this;
 }
-Complex operator+ (const Complex& lhs, const Complex& rhs)
+
+Complex& Complex::operator+(const Complex& lhs, const Complex& rhs)
 {
-    return Complex(lhs.re + rhs.re, lhs.im + rhs.im);
+    Complex sum(lhs);
+    sum += rhs;
+    return sum;
 }
 
-Complex& Complex::operator-= (const Complex& rhs)
+Complex& Complex::operator-=(const Complex& rhs)
 {
     re -= rhs.re;
     im -= rhs.im;
     return *this;
 }
-Complex operator- (const Complex& lhs, const Complex& rhs)
+Complex operator-(const Complex& lhs, const Complex& rhs)
 {
     return Complex(lhs.re - rhs.re, lhs.im - rhs.im);
 }
 
-Complex& Complex::operator*= (const Complex& rhs) {
-    double per = re * rhs.re - im * rhs.im;
-    im = re * rhs.im + im * rhs.re;
-    re = per;
+Complex& Complex::operator*=(const double rhs) {
+    re *= rhs;
+    im *= rhs;
     return *this;
 }
-Complex operator* (const Complex& lhs, const Complex& rhs)
+
+Complex& Complex::operator*(const Complex& lhs, const Complex& rhs)
 {
     return Complex(lhs.re * rhs.re - lhs.im * rhs.im, lhs.re * rhs.im + lhs.im * rhs.re);
 }
@@ -54,13 +61,13 @@ Complex operator/ (const Complex& lhs, const Complex& rhs) {
     return Complex((lhs.re * rhs.re + lhs.im * rhs.im) / (rhs.re * rhs.re + rhs.im * rhs.im), (rhs.re * lhs.im - lhs.re * rhs.im) / (rhs.re * rhs.re + rhs.im * rhs.im));
 }
 
-std::ostream& Complex::writeTo(std::ostream& ostrm) const
+std::ostream& Complex::WriteTo(std::ostream& ostrm) const
 {
     ostrm << leftBrace << re << separator << im << rightBrace;
     return ostrm;
 }
 
-std::istream& Complex::readFrom(std::istream& istrm)
+std::istream& Complex::ReadFrom(std::istream& istrm)
 {
     char leftBrace(0);
     double real(0.0);
