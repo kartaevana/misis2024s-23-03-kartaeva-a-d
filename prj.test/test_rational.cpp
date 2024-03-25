462fb37dd9d2bc8c279354ed129455786ae26622
kartaevana@yandex.ru <kartaevana@yandex.ru>
2024-03-25 11:06:14 +0300

    edit test_rational.cpp

--- prj.test/test_rational.cpp
+++ prj.test/test_rational.cpp
@@ -1,108 +1,105 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
+#include <rational/rational.hpp>
 
-#include "rational.hpp"
+TEST_CASE("rational ctor") {
+Rational r_def;
+CHECK(0 == r_def.num());
+CHECK(1 == r_def.den());
 
-TEST_CASE("Testing rational constructor") {
-	Rational r_empty;
-	CHECK(r_empty.Num() == 0);
-	CHECK(r_empty.Den() == 1);
+Rational r_int(3);
+CHECK(3 == r_int.num());
+CHECK(1 == r_int.den());
 
-	Rational r_5(5);
-	CHECK(r_5.Num() == 5);
-	CHECK(r_5.Den() == 1);
-
-	CHECK_THROWS(Rational(2, 0));
-
-	Rational r1(3, 4);
-	CHECK(r1.Num() == 3);
-	CHECK(r1.Den() == 4);
+CHECK_THROWS(Rational(1, 0));
 }
 
-TEST_CASE("Testing comparision operators") {
-	Rational r1(3, 4), r2(4, 3);
+TEST_CASE("rational arithmetic") {
+Rational r_positive(3, 2);
+Rational r_negative(-2, 6);
+Rational sum;
+sum = r_positive + r_negative;
 
-	CHECK(r1 == Rational(3, 4));
-	CHECK(r1 == Rational(178954776, 238606368));
-	CHECK(!(r1 == Rational(4, 3)));
-	CHECK(!(r1 == Rational(178954775, 238606368)));
+CHECK(7 == sum.num());
+CHECK(6 == sum.den());
 
-	CHECK(r1 != Rational(4, 3));
-	CHECK(r1 != Rational(178954775, 238606368));
-	CHECK(!(r1 != Rational(3, 4)));
-	CHECK(!(r1 != Rational(178954776, 238606368)));
+sum = sum + 5;
 
-	CHECK(r1 > Rational(178954775, 238606368));
-	CHECK(r1 < Rational(178954777, 238606368));
-	CHECK(r1 >= Rational(178954775, 238606368));
-	CHECK(r1 <= Rational(178954777, 238606368));
-	CHECK(r1 <= Rational(178954776, 238606368));
-	CHECK(r1 >= Rational(178954776, 238606368));
+CHECK(37 == sum.num());
+CHECK(6 == sum.den());
+
+Rational dif;
+dif = r_positive - r_negative;
+
+CHECK(11 == dif.num());
+CHECK(6 == dif.den());
+
+dif = dif - 1;
+
+CHECK(5 == dif.num());
+CHECK(6 == dif.den());
+
+Rational prod;
+prod = r_negative * r_positive;
+
+CHECK(-1 == prod.num());
+CHECK(2 == prod.den());
+
+prod = prod * (-2);
+
+CHECK(1 == prod.num());
+CHECK(1 == prod.den());
+
+Rational div;
+div = r_positive/r_negative;
+
+CHECK(-9 == div.num());
+CHECK(2 == div.den());
+
+div = div / (-3);
+
+CHECK(3 == div.num());
+CHECK(2 == div.den());
+
+Rational r_zero(0, 1);
+
+CHECK_THROWS(Rational(1, 0));
+CHECK_THROWS(r_negative / r_zero);
 }
 
-TEST_CASE("Maths") {
-	Rational r1(5, 2), r2(3, 2), r3(1, 2), r4(4, 1), r5(4, 3);
-	CHECK(r1 == Rational(2, 1) + Rational(1, 2));
-	CHECK(r1 == Rational(3, 2) + int64_t(1));
-	CHECK(r1 == int64_t(2) + Rational(1, 2));
+TEST_CASE("rational IO") {
+std::istringstream istream("2/3");
+Rational reading;
 
-	CHECK(r2 == Rational(2, 1) - Rational(1, 2));
-	CHECK(r3 == Rational(3, 2) - int64_t(1));
-	CHECK(r2 == int64_t(2) - Rational(1, 2));
+istream >> reading;
+CHECK_FALSE(istream.fail());
+CHECK(reading.num() == 2);
+CHECK(reading.den() == 3);
 
-	CHECK(r4 == Rational(8, 1) * Rational(1, 2));
-	CHECK(r2 == Rational(3, 4) * int64_t(2));
-	CHECK(r4 == int64_t(8) * Rational(1, 2));
+std::istringstream istream2("16/8");
+istream2 >> reading;
+CHECK(reading.num() == 2);
+CHECK(reading.den() == 1);
 
-	CHECK(r4 == Rational(2, 1) / Rational(1, 2));
-	CHECK(r5 == Rational(8, 3) / int64_t(2));
-	CHECK(r5 == int64_t(2) / Rational(3, 2));
-}
+std::istringstream istream3("    7/8");
+istream3 >> reading;
+CHECK(reading.num() == 7);
+CHECK(reading.den() == 8);
 
-TEST_CASE("Maths with assignment") {
-	Rational r1(4, 3), r2(5, 7);
-	int64_t i1(2);
+std::istringstream istream4("7/ 8");
+istream4 >> reading;
+CHECK(istream4.fail());
 
-	r1 += r2;
-	CHECK(r1 == Rational(43, 21));
-	r1 += i1;
-	CHECK(r1 == Rational(85, 21));
+std::istringstream istream5("4/-54");
+istream5 >> reading;
+CHECK(istream5.fail());
 
-	r1 -= r2;
-	CHECK(r1 == Rational(10, 3));
-	r1 -= i1;
-	CHECK(r1 == Rational(4, 3));
+std::ostringstream ostream1;
 
-	r1 *= r2;
-	CHECK(r1 == Rational(20, 21));
-	r1 *= i1;
-	CHECK(r1 == Rational(40, 21));
+ostream1 << Rational(2, 3);
+CHECK(ostream1.str() == "2/3");
 
-	r1 /= r2;
-	CHECK(r1 == Rational(8, 3));
-	r1 /= i1;
-	CHECK(r1 == Rational(4, 3));
-}
-
-TEST_CASE("Input test") {
-	Rational r1;
-	std::cout << "Input rational in format 4/3: ";
-	std::cin >> r1;
-	CHECK(r1 == Rational(4, 3));
-
-	std::cout << "Input rational in format -2/3: ";
-	std::cin >> r1;
-	CHECK(r1 == Rational(-2, 3));
-
-	std::cout << "Input rational in format 0/n (n is any integer): ";
-	std::cin >> r1;
-	CHECK(r1 == Rational(0, 1));
-
-	std::cout << "Input rational in format n/0 (n is any integer): ";
-	CHECK_THROWS(std::cin >> r1);
-}
-
-TEST_CASE("Extra simplification test") {
-	Rational r1(15823399283712, -101279287584);
-	CHECK(r1 == Rational(-2656, 17));
+std::ostringstream ostream2;
+ostream2 << Rational(-2, 3);
+CHECK(ostream2.str() == "-2/3");
 }
